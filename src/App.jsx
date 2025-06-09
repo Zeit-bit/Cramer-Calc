48044c5e90cabb3c5fcead3954732e73c4433c00
Zeit-bit <reyesalvarez.patricio@gmail.com>
2025-06-09 01:13:28 -0600

    Add recursive function to calculate and render determinant of a matrix

--- src/App.jsx
+++ src/App.jsx
@@ -36,6 +36,7 @@ const App = () => {
                 key={`row-${rIndex}_col-${cIndex}`}
                 value={col}
                 placeholder="0"
+                required
                 onChange={(e) => {
                   HandleMatrix(e, rIndex, cIndex)
                 }}
@@ -61,6 +62,7 @@ const App = () => {
               key={`a_row-${rIndex}`}
               value={row}
               placeholder="0"
+              required
               onChange={(e) => {
                 HandleAugmentation(e, rIndex)
               }}
@@ -107,7 +109,10 @@ const App = () => {
     setAugmentation(null)
   }
 
-  const HandleSolve = () => {}
+  const HandleSolve = (e) => {
+    e.preventDefault()
+    setSolve(true)
+  }
 
   const emptyMatrix = CreateEmptyMatrix(size)
   const emptyAugmentation = CreateEmptyAugmentation(size)
@@ -119,6 +124,7 @@ const App = () => {
   const renderingAugmentation = MapAugmentation(
     augmentation ? augmentation : emptyAugmentation,
   )
+
   return (
     <div className="flex h-screen flex-col items-center">
       <h1 className="mt-5 border-4 p-2 text-3xl font-bold">
@@ -144,43 +150,137 @@ const App = () => {
       </div>
 
       <div className="flex flex-col gap-5 border-4 p-5">
-        <div className="flex gap-4">
-          {renderingMatrix}
-          {renderingAugmentation}
-        </div>
-        <button
-          onClick={() => {
-            setSolve(true)
-          }}
-          className="flex items-center justify-center border-2 p-2 hover:border-blue-700 hover:bg-gray-800 hover:text-white active:bg-blue-500"
-        >
-          Solve
-        </button>
+        <form onSubmit={(e) => HandleSolve(e)}>
+          <div className="flex gap-4">
+            {renderingMatrix}
+            {renderingAugmentation}
+          </div>
+          <button className="m-auto mt-5 flex w-full items-center justify-center border-2 p-2 hover:border-blue-700 hover:bg-gray-800 hover:text-white active:bg-blue-500">
+            Solve
+          </button>
+        </form>
       </div>
 
-      <div className="m-5 border-4 p-2">
-        {solve ? <Solution matrix={matrix} /> : <h2>Pasos</h2>}
-      </div>
+      {solve ? <Solution matrix={matrix} /> : null}
     </div>
   )
 }
 
 const Solution = ({ matrix }) => {
-  const expMatrix = JSON.stringify(
-    matrix.map((row) => row.map((col) => Number(col))),
-  )
-  
+  const integerMatrix = matrix.map((r) => r.map((c) => parseFloat(c)))
+  const Matrix2Latex = (matrix) => {
+    let latex = ""
+    const rows = matrix.map((r) => {
+      let update = ""
+      for (let i = 0; i < matrix.length; i++) {
+        update += `${r[i]}${i < matrix.length - 1 ? " & " : ""}`
+      }
+      return update
+    })
+
+    for (let i = 0; i < matrix.length; i++) {
+      latex += `${rows[i]}${i < matrix.length - 1 ? " \\\\ " : ""}`
+    }
+
+    return `\\left| \\begin{matrix} ${latex} \\end{matrix} \\right|`
+  }
+
+  const GetDet = (matrix, key = [1], name = "") => {
+    const classStyle = "border p-2 ml-10 mr-10 mt-2 md-2"
+    if (matrix.length === 1) {
+      return (
+        <div className={classStyle}>
+          <MathJax>{`\\[${name} ${Matrix2Latex(matrix)} = ${matrix[0][0]}\\]`}</MathJax>
+        </div>
+      )
+    }
+    if (matrix.length === 2) {
+      const result = matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+      const steps = `${matrix[0][0]} * ${matrix[1][1]} - ${matrix[0][1]} * ${matrix[1][0]}`
+      return [
+        result,
+        <div className={classStyle}>
+          <MathJax>{`\\[${name} ${Matrix2Latex(matrix)} = ${steps} = ${result}\\]`}</MathJax>
+        </div>,
+      ]
+    }
+    if (matrix.length === 3) {
+      const result =
+        matrix[0][0] * matrix[1][1] * matrix[2][2] +
+        matrix[1][0] * matrix[2][1] * matrix[0][2] +
+        matrix[0][1] * matrix[1][2] * matrix[2][0] -
+        (matrix[0][2] * matrix[1][1] * matrix[2][0] +
+          matrix[1][2] * matrix[2][1] * matrix[0][0] +
+          matrix[0][1] * matrix[1][0] * matrix[2][2])
+      const steps = `
+        (${matrix[0][0]} * ${matrix[1][1]} * ${matrix[2][2]} +
+        ${matrix[1][0]} * ${matrix[2][1]} * ${matrix[0][2]} +
+        ${matrix[0][1]} * ${matrix[1][2]} * ${matrix[2][0]}) -
+        (${matrix[0][2]} * ${matrix[1][1]} * ${matrix[2][0]} +
+          ${matrix[1][2]} * ${matrix[2][1]} * ${matrix[0][0]} +
+          ${matrix[0][1]} * ${matrix[1][0]} * ${matrix[2][2]})`
+      return [
+        result,
+        <div className={classStyle} key={key++}>
+          <MathJax>{`\\[${name} ${Matrix2Latex(matrix)} = ${steps} = ${result} \\]`}</MathJax>
+        </div>,
+      ]
+    }
+
+    let steps = `\\[${name} ${Matrix2Latex(matrix)} = `
+    const children = []
+    let result = 0
+
+    const removedRow = matrix.filter((_, rI) => rI != 0)
+    for (let i = 0; i < matrix.length; i++) {
+      const sign = i % 2 === 0 ? +1 : -1
+      const minor = removedRow.map((r) => r.filter((_, cI) => cI != i))
+      steps += `${sign === +1 ? "+" : "-"} ${matrix[0][i]} ${Matrix2Latex(minor)}`
+      const [childrenResult, childrenJSX] = GetDet(minor, ++key)
+      children.push(childrenJSX)
+      result += sign * matrix[0][i] * childrenResult
+    }
+    steps += `= ${result} \\]`
+
+    return [result, <DetStep key={key++} latex={steps} children={children} />]
+  }
+
+  const [deltaResult, deltaRender] = GetDet(integerMatrix, 1, "\\Delta =")
+  const [showDelta, setShowDelta] = useState(false)
   return (
-    <div>
+    <div className="m-5 border-4 p-2">
       <MathJaxContext>
-        <h2>Pasos</h2>
-        <p>{expMatrix}</p>
+        <h2 className="border p-2 font-bold">Pasos</h2>
         <MathJax>
           {`\\[
-            \\Delta = \\left| \\begin{matrix} 1 & 2 \\\\ 2 & 2 \\end{matrix} \\right|
+            \\Delta = ${Matrix2Latex(integerMatrix)} = ${deltaResult}
           \\]`}
         </MathJax>
+        <button
+          className="border pr-2 pl-2 hover:bg-gray-200 active:bg-gray-400"
+          onClick={() => setShowDelta(!showDelta)}
+        >
+          Show details
+        </button>
+        {showDelta && deltaRender}
       </MathJaxContext>
+    </div>
+  )
+}
+
+const DetStep = ({ latex, children }) => {
+  const [open, setOpen] = useState(false)
+
+  return (
+    <div className="mt-2 mr-10 ml-10 border p-2">
+      <MathJax>{latex}</MathJax>
+      <button
+        onClick={() => setOpen(!open)}
+        className="mb-2 border px-2 hover:bg-gray-200 active:bg-gray-400"
+      >
+        {open ? "hide details" : "show details"}
+      </button>
+      {open && children}
     </div>
   )
 }
